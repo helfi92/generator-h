20ceea7096d9aaa037f7a5877e1a606fa9146a2f
helfi92 <helfi92@gmail.com>
2016-12-26 11:42:40 -0500

    add prompting

--- generators/app/index.js
+++ generators/app/index.js
@@ -7,6 +7,22 @@ module.exports = class extends Generator {
 		this.option('babel');
 	}
 
+	prompting() {
+		return this.prompt([{
+			type    : 'input',
+			name    : 'name',
+			message : 'Your project name',
+			default : this.appname // Default to current folder name
+		}, {
+			type    : 'confirm',
+			name    : 'cool',
+			message : 'Would you like to enable the Cool feature?'
+		}]).then((answers) => {
+			this.log('app name', answers.name);
+			this.log('cool feature', answers.cool);
+		});
+	}
+
 	method1() {
 		console.log('method 1 just ran, wohoo');
 	}
